b548c0e7e1f7cf2f5b912db6f6b0c2966d1fcbe5
Gdel-J <jessygdelvecchio@gmail.com>
2023-09-18 12:59:13 -0400

    fixing issue25

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const allRoutes = require("./controllers");
 const session = require("express-session");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-require('dotenv').config();
 const apiRoutes = require("./controllers/api");
 const frontendRoutes = require("./controllers/frontendRoutes");
+const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,16 +27,17 @@ const sess = {
 };
 app.use(session(sess));
 
-app.use(express.static('public'));
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+}
 
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 app.use("/api", apiRoutes);
 app.use("/", frontendRoutes);
-
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -45,8 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, 'localhost', function() {
+  app.listen(PORT, function() {
     console.log(`App listening on http://localhost:${PORT}`);
-});
-
+  });
 });
